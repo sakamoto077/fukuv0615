03ae1a96fe5cf6c1932337bcfdf98f78cb6e735a
sakamoto <130636519+sakamoto077@users.noreply.github.com>
2023-06-15 14:01:06 +0900

    0615復習

--- fukuv0615/Form1.Designer.cs
+++ fukuv0615/Form1.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(294, 190);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "0";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(432, 130);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "cheack";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Button button1;
     }
 }
--- fukuv0615/Form1.cs
+++ fukuv0615/Form1.cs
@@ -2,9 +2,39 @@ namespace fukuv0615
 {
     public partial class Form1 : Form
     {
+        static Random rand = new Random();
+        int ans = $ "{rand.Next(0, 100)}";
+        int count = rand.Next(0);
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count++;
+            MessageBox.Show("{count}âÒñ⁄ÇÃíßêÌ");
+            int guees;
+
+            if (ans == guees)
+            {
+                MessageBox.Show("Bingo!!");
+            }
+            if (ans <= guees)
+            {
+                MessageBox.Show("Less");
+            }
+            if (ans >= guees)
+            {
+                MessageBox.Show("Greater");
+            }
+        }
+
     }
 }
